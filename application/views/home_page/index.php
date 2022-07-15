55fed89ac6f5acc7689dfbb3aac882980c224dfe
Rizky Naufal <naufalrizky79@gmail.com>
2022-07-15 08:22:49 +0700

    2022

--- application/views/home_page/index.php
+++ application/views/home_page/index.php
@@ -108,7 +108,7 @@
   <div class=" flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 ">
     <!-- Copyright -->
     <div class="text-white mb-3 mb-md-0">
-      Copyright ©Tately NV. 2020. All rights reserved.
+      Copyright ©Tately NV. 2022. All rights reserved.
     </div>
   </div>
 </footer>
